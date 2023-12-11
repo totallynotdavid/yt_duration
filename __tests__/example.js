d971306d2a1139300856fa82a79a66383c68d62e
David Duran <dadch1404@gmail.com>
2023-12-11 06:37:12 -0500

    Update video duration tests

--- __tests__/example.js
+++ __tests__/example.js
@@ -1,18 +1,23 @@
 const { getVideoLength } = require("../src/index")
 
 describe("getVideoLength", () => {
-  it("debe de retornar la duración correcta en segundos del video con ID in8JE5E4NFI", async () => {
+  it("should return the correct human-readable duration of the video with ID in8JE5E4NFI", async () => {
     const duration = await getVideoLength("in8JE5E4NFI")
-    expect(duration).toBe(3630)
+    expect(duration).toBe("01:00:30") // El video dura 1 hora con 30 segundos
   })
 
-  it("debe de retornar la duración correcta en minutos del video con ID 8JE5E4NFI", async () => {
+  it("should return the correct duration in seconds of the video with ID in8JE5E4NFI", async () => {
+    const duration = await getVideoLength("in8JE5E4NFI", "seconds")
+    expect(duration).toBe(3630) // 1 hora con 30 segundos en segundos
+  })
+
+  it("should return the correct duration in minutes of the video with ID in8JE5E4NFI", async () => {
     const duration = await getVideoLength("in8JE5E4NFI", "minutes")
-    expect(duration).toBeCloseTo(60.5)
+    expect(duration).toBeCloseTo(60.5) // 1 hora con 30 segundos en minutos
   })
 
-  it("debe de retornar la duración correcta en horas del video con ID 8JE5E4NFI", async () => {
+  it("should return the correct duration in hours of the video with ID in8JE5E4NFI", async () => {
     const duration = await getVideoLength("in8JE5E4NFI", "hours")
-    expect(duration).toBeCloseTo(1.00805)
+    expect(duration).toBeCloseTo(1.008333) // 1 hora con 30 segundos en horas
   })
 })
