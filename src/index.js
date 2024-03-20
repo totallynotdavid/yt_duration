bf63d987eb46b5b66a52b12fccd62d76fff016f4
David Duran <dadch1404@gmail.com>
2024-03-19 23:00:08 -0500

    refactor: API key rotation logic into separate module

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
-require("dotenv").config()
-
-// Retrieve all environment variables that start with "YOUTUBE_API_KEY"
-const apiKeys = Object.entries(process.env) // eslint-disable-line no-undef
-  .filter(([key]) => key.startsWith("YOUTUBE_API_KEY"))
-  .map(([, value]) => value)
-
-let currentApiKeyIndex = 0
-
-/**
- * Returns the next API key in the rotation.
- * @returns {string} The next API key.
- */
-function getNextApiKey() {
-  const apiKey = apiKeys[currentApiKeyIndex]
-  currentApiKeyIndex = (currentApiKeyIndex + 1) % apiKeys.length
-  return apiKey
-}
+const { getNextApiKey } = require("./apiKeyRotation")
 
 /**
  * Retrieves the length of a YouTube video.
